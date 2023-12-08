a727443787599a7cd0520344831c099453ed70e9
Levi Oliveira <leviedez@gmail.com>
2023-12-07 23:17:05 -0300

    Adicionando como ocorre
    Aqui eu explico como o código funciona, mas vai estar tudo resumido no arquivo Readme

--- questao1.c
+++ questao1.c
@@ -3,12 +3,15 @@
 int main() {
     int a, b, c, d;
     
+    //Aqui é onde ocorre a entrada dos valores
     scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    //Aqui é onde ocorre a verificação das condições pedidas pelo BeeCrowd e logo depois, fala se cumpre os requisitos ou não
     if (b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 == 0) {
         printf("Valores aceitos\n");
     } else {
         printf("Valores nao aceitos\n");
     }
-
+    
     return 0;
 }
